220de70f024e4dedccee7efe133d46866fb85475
JoseMiguelBaltazarAvelino06 <josemiguelbaltazaravelino@gmail.com>
2024-10-23 12:02:40 -0600

    Se agrega codigo del Examen HTML, CSS y JavaScript

---
+++ js/script.js
@@ -0,0 +1,26 @@
+def function mostrar:
+    const tareas = [
+    {nombre: "tarea 1", completada: true},
+    {nombre: "tarea 2", completada: true},
+    {nombre: "tarea 3", completada: false},
+    {nombre: "tarea 4", completada: false},
+    {nombre: "tarea 5", completada: true},
+   ];
+
+
+def funtion contarTareas:
+    const arraytareas = {
+        nombre: true,
+        nombre: false
+    }
+
+    const obj = [
+        {completadas: <número de tareas completadas>,
+        noCompletadas: <número de tareas no completadas>
+        }
+
+    ]
+
+def funtion actualizar {
+    const tareas = update
+}
